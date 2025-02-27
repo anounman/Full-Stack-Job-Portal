88932678dac0c948c35ee60d246b40fadc47a045
anounman <ankush.pentester@gmail.com>
2025-02-27 17:51:18 +0100

    deployment fix

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
   },
   build: {
     rollupOptions: {
-      external: ['zwitch'],
+      // external: ['zwitch'],
       output: {
         manualChunks: {
           vendor: ['react', 'react-dom', 'react-router-dom'],
